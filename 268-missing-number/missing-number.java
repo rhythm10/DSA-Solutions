144d27e8e1a4d4041d9fd67933507cc4c0364393
Rhythm Sehgal <rhythmsehgal9@gmail.com>
2024-04-05 13:43:18 +0530

    Time: 0 ms (100.00%) | Memory: 45.5 MB (19.55%) - LeetSync

---
+++ 268-missing-number/missing-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        
+        int n = nums.length;
+        int res = (n * (n+1))/2;
+        int sum = 0;
+        
+        for(int i=0;i<n;i++)
+        {
+            sum += nums[i];
+        }
+
+        return res - sum;
+
+
+
+        
+    }
+}
