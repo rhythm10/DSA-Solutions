2337453d31fc4fedf1447f558faab732b711bdcd
Rhythm Sehgal <rhythmsehgal9@gmail.com>
2024-08-31 15:00:34 +0530

    Time: 5 ms (99.88%) | Memory: 46.3 MB (7.22%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -1,6 +1,8 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         
+        if(matrix == null || matrix.length == 0) return false;
+        
         int n = matrix.length - 1;
         int m = matrix[0].length - 1;
         int i=0,j=m;
