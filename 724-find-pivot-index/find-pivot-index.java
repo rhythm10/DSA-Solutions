d46f2f156b1ce9956edc0389008d4044040d25d2
Rhythm Sehgal <rhythmsehgal9@gmail.com>
2024-09-01 13:23:11 +0530

    Time: 1 ms (93.39%) | Memory: 45.9 MB (13.01%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        if(nums.length < 1) {
+            return -1;
+        }
+
+        int[] pref = new int[nums.length];
+        pref[0] = nums[0];
+
+        for(int i=1;i<nums.length;i++){
+            pref[i] = nums[i] + pref[i-1];
+        }
+
+        for(int i=0;i<pref.length;i++) {
+            if(i==0) {
+                if((pref[pref.length-1] - pref[0]) == 0) {
+                    return i;
+                }
+                continue;
+            }
+            if(i == pref.length-1) {
+                if(pref[pref.length-2] == 0) {
+                    return i;
+                }
+                continue;
+            }
+            int leftSum = pref[i-1];
+            int rightSum = pref[pref.length-1] - pref[i];
+
+            if(leftSum == rightSum) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
