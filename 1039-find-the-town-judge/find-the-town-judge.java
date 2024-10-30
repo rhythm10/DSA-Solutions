7808b99a6120dc25c16e433e6a148cda0561b4c7
Rhythm Sehgal <rhythmsehgal9@gmail.com>
2024-10-30 09:35:57 +0530

    Time: 2 ms (99.71%) | Memory: 53.7 MB (58.74%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.java
+++ 1039-find-the-town-judge/find-the-town-judge.java
@@ -1,17 +1,18 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
-        int[] arr = new int[n+1];
-        for (int i = 0;i<trust.length;i++) {
-            arr[trust[i][1]]++;
-            arr[trust[i][0]]--;
+        int indegree[]=new int[n+1];
+        int outdegree[]=new int[n+1];
+        for(int i=0;i<trust.length;i++)
+        {
+            
+          indegree[trust[i][1]]++;
+          outdegree[trust[i][0]]++;
         }
-        int x = -1;
-        for (int i = 1;i<arr.length;i++) {
-            if (arr[i] == n-1) {
-                x = i;
-            }
+        for(int i=1;i<=n;i++)
+        {
+            if(indegree[i]==n-1 && outdegree[i]==0)
+            return i;
         }
-        
-        return x;
+        return -1;
     }
 }
