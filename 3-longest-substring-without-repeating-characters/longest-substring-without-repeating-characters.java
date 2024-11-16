d12b6988d36dce1d95028146fa2fd0c945c9f23f
Rhythm Sehgal <rhythmsehgal9@gmail.com>
2024-11-16 11:48:21 +0530

    Time: 7 ms (39.14%) | Memory: 44.6 MB (45.91%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -9,16 +9,11 @@ class Solution {
             return 0;
         }
 
-        int[] arr = new int[128];
-
-        // arr[s.charAt(0) - 'a']++;
-
         set.add(s.charAt(0));
         
         for(j=1;j<s.length();j++) {
             while(set.contains(s.charAt(j))) {
                 set.remove(s.charAt(i));
-                // arr[s.charAt(i) - 'a']--;
                 i++;
                 curr--;
             }
@@ -27,7 +22,6 @@ class Solution {
                 maxRes = curr;
             }
             set.add(s.charAt(j));
-            // arr[s.charAt(j) - 'a']++;
         }
 
         return maxRes;
