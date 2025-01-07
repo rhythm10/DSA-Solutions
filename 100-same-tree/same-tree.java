d9c9c0b05ee801757f5d4db0b7e49b90d883b807
Rhythm Sehgal <rhythmsehgal9@gmail.com>
2025-01-07 23:30:06 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (96.65%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -15,12 +15,23 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p == null && q == null) {
-            return true;
-        }
-        if(p == null || q == null) {
+
+        if(p == null && q == null) return true;
+
+        if(p == null || q == null) return false;
+
+        if(p.val != q.val) {
             return false;
         }
-        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+
+        if(!isSameTree(p.left, q.left)) {
+            return false;
+        }
+
+        if(!isSameTree(p.right, q.right)) {
+            return false;
+        }
+
+        return true;
     }
 }
